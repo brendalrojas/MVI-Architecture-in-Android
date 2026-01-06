833295892bcb9c17811b4bb755f3a5feab5fbf3d
Brenda Rojas <80980736+brenRojas@users.noreply.github.com>
2026-01-05 21:20:20 -0600

    add dependencies project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,14 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson)
+    implementation(libs.coroutines)
+    implementation(libs.coroutines.core)
+    implementation(libs.coil)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
